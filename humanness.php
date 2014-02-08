8bd13eacc651b9dfbe232ced39da1bb2b22511db
deep-introspection <deep@introspection.eu>
2014-02-08 14:49:30 -0500

    Finish the results visualization with d3.js!

--- humanness.php
+++ humanness.php
@@ -1,32 +1,82 @@
 <?php
-// load in mysql server configuration (connection string, user/pw, etc)
-include 'mysqlConfig.php';
+
+include "db.php";
+
 // connect to the database
-@mysql_select_db($dsn) or die( "Unable to select database");
+@mysql_select_db("morphome-vpi") or die( "Unable to select database");
  
-// reads the map db
- 
-$query="SELECT a, k, humanness FROM answers";
-mysql_query($query);
- 
-$result = mysql_query($query,$link) or die('Errant query: '.$query);
- 
-// outputs the db as lines of text.
 header('Content-type: text/plain; charset=us-ascii');
-$i=0;
-$line="";
- 
-if(mysql_num_rows($result)) {
- while($value = mysql_fetch_assoc($result)) {
- 
-$line=$line.$value["height"];
- $i=$i+1;
- if ($i==52) {
- $i=0;
- echo $line."\n";
- $line="";}
- else {$line=$line.",";}
- }
+echo 'a	k	humanness
+'; 
+
+// reads the map db
+
+for ($stepA =0.3; $stepA  <= 3; $stepA=$stepA+0.3 )
+	{
+	for ($stepK =0; $stepK  <= 1; $stepK=$stepK+0.1 )
+		{
+			$stepA2 = $stepA + 0.3;
+			$stepK2 = $stepK + 0.1;
+		$query="SELECT a, k, humanness FROM answers WHERE a >= $stepA AND a < $stepA2 AND k >= $stepK AND  k < $stepK2 and mu < 0";
+		$result = mysql_query($query) or die('Errant query: '.$query);
+		$humannessSum=0.0;
+		$humannessAverage=0.0; 
+		// outputs the db as lines of text.
+		while ($value = mysql_fetch_assoc($result))
+		{
+			$humannessSum=$humannessSum+$value['humanness'];
+		}
+		$humannessAverage=($humannessSum?$humannessSum/count($value):0);
+	     echo $stepA*-1;
+	     echo '	' .$stepK;
+ 	     echo '	' . $humannessAverage .'
+';
+	}
 }
-mysql_close();
+
+for ($stepA =0.3; $stepA  <= 3; $stepA=$stepA+0.3 )
+	{
+	for ($stepK =0; $stepK  <= 1; $stepK=$stepK+0.1 )
+		{
+			$stepA2 = $stepA + 0.3;
+			$stepK2 = $stepK + 0.1;
+		$query="SELECT a, k, humanness FROM answers WHERE a >= $stepA AND a < $stepA2 AND k >= $stepK AND  k < $stepK2 and mu > 0";
+		$result = mysql_query($query) or die('Errant query: '.$query);
+		$humannessSum=0.0;
+		$humannessAverage=0.0; 
+		// outputs the db as lines of text.
+		while ($value = mysql_fetch_assoc($result))
+		{
+			$humannessSum=$humannessSum+$value['humanness'];
+		}
+		$humannessAverage=($humannessSum?$humannessSum/count($value):0);
+	     echo $stepA;
+	     echo '	' .$stepK;
+ 	     echo '	' . $humannessAverage .'
+';
+	}
+}
+
+for ($stepK =0; $stepK  <= 1; $stepK=$stepK+0.1 )
+	{
+		$stepA = 0;
+		$stepA2 = 0.3;
+		$stepK2 = $stepK + 0.1;
+	$query="SELECT a, k, humanness FROM answers WHERE a >= $stepA AND a < $stepA2 AND k >= $stepK AND  k < $stepK2";
+	$result = mysql_query($query) or die('Errant query: '.$query);
+	$humannessSum=0.0;
+	$humannessAverage=0.0; 
+	// outputs the db as lines of text.
+	while ($value = mysql_fetch_assoc($result))
+	{
+		$humannessSum=$humannessSum+$value['humanness'];
+	}
+	$humannessAverage=($humannessSum?$humannessSum/count($value):0);
+     echo $stepA;
+     echo '	' .$stepK;
+     echo '	' . $humannessAverage .'
+';
+}
+
+mysql_close($conn);
 ?>
